704961aae53e993f29bfc30360b88a03ac25e5c5
joychiu <joy.chiu@citiesocial.com>
2025-11-01 11:16:56 +0800

    feat: day 9

---
+++ day9.js
@@ -0,0 +1,17 @@
+function isAnagram(s, t) {
+  let count = 0;
+  if (s.length !== t.length) return false;
+  const countS = {};
+  const countT = {};
+  for (let i = 0; i < s.length; i++) {
+    countS[s[i]] = (countS[s[i]] || 0) + 1;
+    countT[t[i]] = (countT[t[i]] || 0) + 1;
+  }
+
+  for (let key in countS) {
+    if (countS[key] !== countT[key]) {
+      return false;
+    }
+  }
+  return true;
+}
