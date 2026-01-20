641f8ea0e57f8caf23adad21bccfdbe91810fb40
Joy chiu <joychiu@JoydeMacBook-Pro.local>
2026-01-20 10:58:43 +0800

    feat: add webpack

--- utils.js
+++ utils.js
@@ -2,7 +2,13 @@ function calculate(n) {
   return ((n * 100 + 20 - 4) % 10) + 3;
 }
 
-module.exports = {
+// CommonJS for node
+// module.exports = {
+//   cal: calculate,
+//   name: "hello",
+// };
+
+export default {
   cal: calculate,
   name: "hello",
 };
